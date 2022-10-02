79eb35a0a74ad8e6ebf359927eb37bb091aba21b
Matias <matso167@gmail.com>
2022-10-02 19:31:03 +0300

    updates

--- dl.sh
+++ dl.sh
@@ -1,6 +1,7 @@
 read -p "Directory name: " dir
 mkdir dl/$dir
-read -p "Paste thumbnails: " input
+echo "Paste thumbnails (then CTRL+D): "
+input=$(</dev/stdin)
 wget --level 2 -o - -nd -r -P dl/$dir -A jpg --follow-tags=img --span-hosts $(echo $input | grep -oP '[^\ ]*.jpg' | sed -e 's/^/https:\/\/xarchivesx.urlgalleries.net\//')
 read -p "Press any key to delete unecessary files from: $PWD/dl/$dir or CTRL+C to cancel" -n 1 -r
 find $PWD/dl/$dir -type f -size -100k -delete
