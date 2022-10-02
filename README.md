a6da9bb11d9ac65470b0089af0bc6d3c05b9efa7
Matias <matso167@gmail.com>
2022-10-02 20:58:43 +0300

    updates

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 Urlgalleries.net downloader for Linux
 
 1) Go to gallery page (urlgalleries.net)
-2) Press CTRL+A
-3) Press CTRL+C 
-4) Run ./dl.sh
-5) Paste clipboard to input
+2) Select and copy thumbnails
+3) Run ./dl.sh
--- dl.sh
+++ dl.sh
@@ -2,6 +2,20 @@ read -p "Directory name: " dir
 mkdir dl/$dir
 echo "Paste thumbnails (then CTRL+D): "
 input=$(</dev/stdin)
-wget --level 2 -o - -nd -r -P dl/$dir -A jpg --follow-tags=img --span-hosts $(echo $input | grep -oP '[^\ ]*.jpg' | sed -e 's/^/https:\/\/xarchivesx.urlgalleries.net\//')
-read -p "Press any key to delete unecessary files from: $PWD/dl/$dir or CTRL+C to cancel" -n 1 -r
-find $PWD/dl/$dir -type f -size -100k -delete
+
+echo ""
+echo "Downloading html documents"
+
+wget -q --level 1 -nd -r -P dl/$dir -A jpg $(echo $input | grep -oP '[^\ ]*.jpg' | sed -e 's/^/https:\/\/xarchivesx.urlgalleries.net\//')
+jpgs=$(cat $(find "$PWD/dl/$dir" -type f -printf "%T@ %Tc %p\n" | sort -n | grep -oP ' \K/.*') | grep -oP 'src="\Khttps://[^"]*.jpg')
+rm dl/$dir/*.jpg
+
+echo "Downloading images"
+
+i=0
+for jpg in $jpgs
+do
+  i=$(($i+1))
+  wget -q -O $PWD/dl/$dir/$i.jpg $jpg &
+  echo "Downloaded $PWD/dl/$dir/$i.jpg"
+done
