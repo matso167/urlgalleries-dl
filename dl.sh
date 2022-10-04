11b6068fc87c95b1c67bfa9d0b1cc7d385d22d22
Matias <matso167@gmail.com>
2022-10-04 22:12:50 +0300

    fix

--- dl.sh
+++ dl.sh
@@ -1,6 +1,10 @@
 dl () {
   local num=$(printf "%03d" $1)
-  local url=$(wget -q -O - $3 | grep -oPi -m 1 'src="\Khttps://[^"]*.(jpg|jpeg)')
+  #echo "get $3"
+  local html=$(wget -q -O - $3)
+  #echo $html > debug/$num.html
+  local url=$(echo $html | grep -oPi -m 1 "src=(\"|')\Khttps://[^(\"|')]*.(jpg|jpeg)")
+  echo "$url --> $PWD/dl/$2/$num.jpg"
   wget -q -O dl/$2/$num.jpg $url
   echo "Downloaded $PWD/dl/$2/$num.jpg"
 }
@@ -9,7 +13,9 @@ read -p "Directory name: " dir
 mkdir dl/$dir
 echo "Paste thumbnails (then CTRL+D): "
 
-echo $(</dev/stdin) | grep -oPi "[^\ ]*.(jpg|jpeg)( |$)" | sed -e 's/^/https:\/\/xarchivesx.urlgalleries.net\//' > links
+input=$(</dev/stdin)
+#echo $input > debug/input.txt
+echo $input | grep -oPi ".*?.(jpg|jpeg)( |$)" | tr -d " " | sed -r 's/#/%23/g' | sed -e 's/^/https:\/\/xarchivesx.urlgalleries.net\//' > links
 i=0
 cat links | while read link
 do
