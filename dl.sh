62bbf788f9e0c586b1e60a1186eb9b5d4f67dd6c
Matias <matso167@gmail.com>
2022-10-06 20:13:14 +0300

    updates

--- dl.sh
+++ dl.sh
@@ -1,17 +1,15 @@
 dl () {
   local num=$(printf "%03d" $1)
   touch dl/$2/$num.jpg.tmp
-  local html=$(wget -q -O - $3)
-  local url=$(echo $html | grep -oPi -m 1 "src=(\"|')\Khttps://[^(\"|')]*.(jpg|jpeg)")
-  #echo "$url --> $PWD/dl/$2/$num.jpg"
+  local url=$(wget -q -O - $3 | grep -oPi -m 1 "src=(\"|')\Khttps://[^(\"|')]*.(jpg|jpeg)")
   wget -q -O dl/$2/$num.jpg $url
-  #echo "Downloaded $PWD/dl/$2/$num.jpg"
   rm dl/$2/$num.jpg.tmp
 }
 
-read -p "Directory name: " dir
+read -p "Gallery URL: " url
+dir="$(echo $url | grep -oP ".*?\K[^\/]+$")-$(date +%s)"
 mkdir dl/$dir
-read -p "Paste gallery URL: " url
+
 input=$(curl "$url&a=10000" -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0')
 echo $input | grep -oPi " href=(\"|')\K[^(\"|\')]*\.(jpg|jpeg)" | tr -d " " | sed -r 's/#/%23/g' | sed -e 's/^/https:\/\/xarchivesx.urlgalleries.net/' > links
 
@@ -24,12 +22,13 @@ done
 
 rm links
 sleep 5
-
-while [ $(ls $PWD/dl/$dir/*.tmp | wc -l) -gt 0 ]
+while [ $(find $PWD/dl/$dir/ -type f -iname *.tmp | wc -l) -gt 0 ]
 do
-  echo "Downloads in progress: $(ls $PWD/dl/$dir/*.tmp | wc -l)"
+  echo -ne "Downloads in progress: $(find $PWD/dl/$dir/ -type f -iname *.tmp | wc -l) \r"
   sleep 1
 done
 
+echo -e '\nDownloads completed'
+
 find $PWD/dl/$dir -type f -size -20k -delete
-echo "ok"
+gpicview $PWD/dl/$dir/$(ls $PWD/dl/$dir | head -n 1)
